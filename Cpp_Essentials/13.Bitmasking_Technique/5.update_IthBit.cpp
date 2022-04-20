d84b8edbb47a52c5d720e4fa719daf1c2fe7fe6e
Mohamed Abdi <mosolves147@gmail.com>
2022-04-20 11:12:47 +0300

    Update Ith Bit

---
+++ Cpp_Essentials/13.Bitmasking_Technique/5.update_IthBit.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void clearIthBit(int &n, int i) {
+    int mask = ~(1<<i);
+    n = n & mask;
+}
+
+void updateIthBit(int &n, int i, int v) {
+    clearIthBit(n, i);
+    int mask = (v<<i);
+    n = n|mask; // sets the right value
+}
+
+int main() {
+
+    int n = 13;
+    int i;
+    cin>>i;
+
+    updateIthBit(n,i,0);
+
+    cout << n;
+
+    return 0;
+}
