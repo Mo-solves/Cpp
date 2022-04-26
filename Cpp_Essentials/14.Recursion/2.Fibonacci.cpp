688b43e74a8d05e5d1cd776dd6561e976a7dd938
Mohamed Abdi <mosolves147@gmail.com>
2022-04-26 15:25:22 +0300

    Fibonacci number

---
+++ Cpp_Essentials/14.Recursion/2.Fibonacci.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n) {
+    // base case
+    if(n==0 or n==1) {
+        return n;
+    }
+    // Recursive case
+    int f1 = fib(n-1);
+    int f2 = fib(n-2);
+    return f1 + f2;
+}
+
+int main() {
+
+    int n;
+    cout << "Enter a number: ";
+    cin>>n;
+
+    cout << fib(n) << endl;
+    return 0;
+}
